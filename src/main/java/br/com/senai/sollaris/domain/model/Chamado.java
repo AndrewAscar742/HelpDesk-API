5fc331f46e67f8206c562426d4ff1ae3587070b2
AndrewAscar742 <andrewmatosascar@outlook.com>
2023-02-12 19:57:49 -0300

    Build(Model): Creation of domain classes

---
+++ src/main/java/br/com/senai/sollaris/domain/model/Chamado.java
@@ -0,0 +1,128 @@
+package br.com.senai.sollaris.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import br.com.senai.sollaris.domain.enums.Prioridade;
+import br.com.senai.sollaris.domain.enums.Status;
+
+public class Chamado {
+	private Integer id;
+	private LocalDateTime dataAbertura = LocalDateTime.now();
+	private LocalDateTime dataFechamento;
+	private Prioridade prioridade;
+	private Status status;
+	private String titulo;
+	private String observacao;
+	
+	private Tecnico tecnico;
+	private Cliente cliente;
+	
+	public Chamado() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Chamado(Integer id, Prioridade prioridade, Status status, String titulo, String observacao, Tecnico tecnico,
+			Cliente cliente) {
+		super();
+		this.id = id;
+		this.prioridade = prioridade;
+		this.status = status;
+		this.titulo = titulo;
+		this.observacao = observacao;
+		this.tecnico = tecnico;
+		this.cliente = cliente;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDateTime getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(LocalDateTime dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public Prioridade getPrioridade() {
+		return prioridade;
+	}
+
+	public void setPrioridade(Prioridade prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public Tecnico getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Tecnico tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Chamado other = (Chamado) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
